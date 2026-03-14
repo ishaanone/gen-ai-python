2ce0e1e791eef42bb510265225f8e67b7d04bc6f
Ishaan Sharma <ishaanone@yahoo.com>
2026-03-14 12:34:39 +0530

    removed redundant code from second-script.py

--- Day_01/second-script.py
+++ Day_01/second-script.py
@@ -9,17 +9,4 @@ print(vegetable)
 print("age")
 print(age)
 print(f"The fruit is {fruit}, the vegetable is {vegetable}, and the age is {age}")
-print("The fruit is", fruit ,"the vegetable is", vegetable, "and the age is", age)
-
-############## from akshat trainer edureka ###################
-fruit = "apple"
-vegetable = "carrot"
-age = 45
-print(fruit)
-print("fruit")
-print(vegetable)
-print("vegetable")
-print(age)
-print("age")
-print(f"The fruit is {fruit}, the vegetable is {vegetable}, and the age is {age}.")
-print("the fruit is :", fruit, "the vegetable is :", vegetable, "and the age is :", age)
+print("The fruit is: ", fruit ,"the vegetable is: ", vegetable, "and the age is: ", age)
